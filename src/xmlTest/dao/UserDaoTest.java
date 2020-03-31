861342a37498c3cc324ab7cdc94d031b3c8ce492
dongKos <dhkim931101@naver.com>
2020-03-31 11:05:22 +0900

    chapter1

--- src/xmlTest/dao/UserDaoTest.java
+++ src/xmlTest/dao/UserDaoTest.java
@@ -15,10 +15,17 @@ public class UserDaoTest {
 //				new AnnotationConfigApplicationContext(DaoFactory.class);
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
+		UserDao dao2 = context.getBean("userDao", UserDao.class);
 		
 		User user = dao.get("id01");
+		User user2 = dao.get("id01");
 		System.out.println(user.getId() + user.getName() + user.getPwd()); 
 		System.out.println(user.toString());
+		System.out.println(user.hashCode());
+		System.out.println(user2.hashCode());
+		
+		System.out.println(dao.hashCode());
+		System.out.println(dao2.hashCode());
 
 	}
 
